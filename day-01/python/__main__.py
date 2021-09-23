ed366a70689700f07051a2e9a4b3bca060c0ec8e
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-23 06:30:23 -0400

    add: python O(n^2) solution

--- day-01/python/__main__.py
+++ day-01/python/__main__.py
@@ -12,16 +12,28 @@ import unittest
 
 ## Functions
 def solution(k: int, list_: list[int]) -> bool:
-    """Returns True if k can be found by a dditionof any 2 numbers in list"""
-    return True
+    """
+    O(n^2)
+    Returns True if k can be found by addition of any 2 numbers in list
+    """
+    for a, c in enumerate(list_):
+        for b, d in enumerate(list_):
+            if a == b:
+                continue
+            if c + d == k:
+                return True
+    return False
 
 
 ## Classes
-class Test_Solution(unittest.TestCase):
+class Sum_of_Numbers(unittest.TestCase):
 
     # -Instance Methods
     def test_solution(self) -> None:
         self.assertTrue(solution(17, [10, 15, 3, 7]))
+        self.assertTrue(solution(5, [1, 2, 4, 8]))
+        self.assertFalse(solution(9, [1, 5, 6]))
+        self.assertFalse(solution(5, [10, 15, 3, 7]))
 
 
 ## Body
