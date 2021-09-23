d42543072ec74eab38467b6d237510f2467d308d
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-23 06:38:35 -0400

    add(python): linear solution

--- day-01/python/__main__.py
+++ day-01/python/__main__.py
@@ -25,6 +25,19 @@ def solution(k: int, list_: list[int]) -> bool:
     return False
 
 
+def solution_linear(k: int, list_: list[int]) -> bool:
+    """
+    O(n)
+    Returns True if k can be found by addition of any 2 numbers in list
+    """
+    potential = set()
+    for a in list_:
+        if a in potential:
+            return True
+        potential.add(k - a)
+    return False
+
+
 ## Classes
 class Sum_of_Numbers(unittest.TestCase):
 
@@ -36,6 +49,14 @@ class Sum_of_Numbers(unittest.TestCase):
         self.assertFalse(solution(10, [5, 6, 8, 9]))
         self.assertFalse(solution(9, [1, 5, 6]))
         self.assertFalse(solution(5, [10, 15, 3, 7]))
+
+    def test_solution_linear(self) -> None:
+        self.assertTrue(solution_linear(17, [10, 15, 3, 7]))
+        self.assertTrue(solution_linear(5, [1, 2, 4, 8]))
+        self.assertTrue(solution_linear(2, [1, 1, 2, 3]))
+        self.assertFalse(solution_linear(10, [5, 6, 8, 9]))
+        self.assertFalse(solution_linear(9, [1, 5, 6]))
+        self.assertFalse(solution_linear(5, [10, 15, 3, 7]))
 
 
 ## Body
